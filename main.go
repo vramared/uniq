eae15c51fa195d9100a71edb3bff50d7632d94c1
Vineet Ramareddi <vramared@ucsc.edu>
2020-05-11 14:22:22 -0700

    don't print identical adjacent lines

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	args := os.Args[1:]
+
+	fd, err := os.Open(args[0])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "reading stderr: ", err)
+	}
+
+	scanner := bufio.NewScanner(fd)
+	fmt.Println("Reading from: ", args[0])
+	prev := ""
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Compare(line, prev) != 0 {
+			fmt.Println(line)
+		}
+		prev = line
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading stderr: ", err)
+	}
 }
