feeb13108002b00a536e1af810bcf227ba18e7d1
Vineet Ramareddi <vramared@ucsc.edu>
2020-05-12 15:43:27 -0700

    command line args

--- main.go
+++ main.go
@@ -2,13 +2,43 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
+const usage = "usage: go run main.go [-c | -d | -u] [-i] [-f fields] [-s chars] [input [output]]"
+
+type flags struct {
+	count    bool
+	repeated bool
+	unique   bool
+}
+
+func processFlags() *flags {
+	count := flag.Bool("c", false, "count mode")
+	repeated := flag.Bool("d", false, "output only repeated lines")
+	unique := flag.Bool("u", false, "output only unique lines")
+	flag.Parse()
+	return &flags{*count, *repeated, *unique}
+}
+
+func validateOpts(f *flags) bool {
+	if (f.count && f.repeated) || (f.count && f.unique) || (f.repeated && f.unique) {
+		return false
+	}
+	return true
+}
+
 func main() {
-	args := os.Args[1:]
+	opts := processFlags()
+	if !validateOpts(opts) {
+		fmt.Println(usage)
+		return
+	}
+	fmt.Println(opts)
+	args := os.Args[2:]
 
 	fd, err := os.Open(args[0])
 	if err != nil {
@@ -16,12 +46,15 @@ func main() {
 	}
 
 	scanner := bufio.NewScanner(fd)
-	fmt.Println("Reading from: ", args[0])
 	prev := ""
+	counter := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		if strings.Compare(line, prev) != 0 {
-			fmt.Println(line)
+		if strings.Compare(line, prev) != 0 && strings.Compare(prev, "") != 0 {
+			fmt.Println("  ", counter, prev)
+			counter = 1
+		} else {
+			counter++
 		}
 		prev = line
 	}
